49e53642d6263c3c9817a35b62801d7e9fada652
janburian <jburian@students.zcu.cz>
2024-01-10 12:04:05 +0100

    Create tile_image_test.py

---
+++ tests/tile_image_test.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pytest
+from pathlib import Path
+import os
+from src import tile_image
+
+def muj_processing(img: np.array) -> np.array:
+    img[::2] = 0
+
+    return img
+
+def test_image_tile():
+    path_to_czi = Path(os.path.join(Path(__file__).parent.parent), 'data_czi', 'J7_5_a.czi')
+    image = tile_image.ImageSplitterMerger(path_to_czi, tilesize_px=200, overlap_px=0, pixelsize_mm=[0.01, 0.01], fcn=muj_processing)
+    merged_image = image.split_and_merge_image()
+
+    assert merged_image.shape[0] > 0
+    assert merged_image.shape[1] > 0
+    assert merged_image[0] == 0
